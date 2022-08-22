92af9b02d55b550ede253c3582ccd4b33f7385dd
citymer <cityhunter8484@gmail.com>
2022-08-22 16:22:13 +0200

    composant titres

---
+++ src/components/Titres.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const titres = () => {
+  return (
+    <div className="titres">
+      <h1>
+        Bonjour <span>Jéjé</span>
+      </h1>
+      <h2>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
+    </div>
+  )
+}
+
+export default titres
