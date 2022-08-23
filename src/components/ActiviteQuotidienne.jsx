55f593ed69a60ada409553826ed9fb670cef3774
citymer <cityhunter8484@gmail.com>
2022-08-23 16:44:19 +0200

    titre activité quotidienne

--- src/components/ActiviteQuotidienne.jsx
+++ src/components/ActiviteQuotidienne.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const ActiviteQuotidienne = () => {
-  return <div className="activite"></div>
+  return (
+    <div className="activite">
+      <div className="presentation">
+        <h3>Activité quotidienne</h3>
+        <div className="contentPoidCalorie">
+          <div className="divpoids">
+            <div className="cercleNoir"></div>
+            <p className="poid">Poids (kg)</p>
+          </div>
+          <div className="divcalorie">
+            <div className="cercleRouge"></div>
+            <p className="calorie">Calories brûlées (kCal)</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ActiviteQuotidienne
