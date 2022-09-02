c15258d15e6d744af2c76fb294270de47e8dafbd
citymer <cityhunter8484@gmail.com>
2022-09-02 11:57:15 +0200

    creation mockAPI

---
+++ src/services/mockAPI.js
@@ -0,0 +1,31 @@
+import {
+  USER_MAIN_DATA,
+  USER_ACTIVITY,
+  USER_AVERAGE_SESSIONS,
+  USER_PERFORMANCE,
+} from '../mocks/data'
+
+const useFetch = (userId) => {
+  const mainData = USER_MAIN_DATA.find((elem) => elem.id === parseInt(userId))
+  const activityData = USER_ACTIVITY.find(
+    (elem) => elem.userId === parseInt(userId)
+  )
+  const averageSessionsData = USER_AVERAGE_SESSIONS.find(
+    (elem) => elem.userId === parseInt(userId)
+  )
+  const performanceData = USER_PERFORMANCE.find(
+    (elem) => elem.userId === parseInt(userId)
+  )
+  const isLoading = null
+  const error = null
+  return {
+    mainData,
+    activityData,
+    averageSessionsData,
+    performanceData,
+    isLoading,
+    error,
+  }
+}
+
+export default useFetch
