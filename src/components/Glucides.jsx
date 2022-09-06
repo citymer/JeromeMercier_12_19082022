f3f92abc8add9655d22364ca667fde3717765d5f
citymer <cityhunter8484@gmail.com>
2022-09-06 17:50:21 +0200

    commentaires glucides.jsx

--- src/components/Glucides.jsx
+++ src/components/Glucides.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import glucides from '../assets/carbs-icon.png'
 
+/**
+ * builds the component that displays the amount of carbohydrate
+ * @param {object} data
+ * @returns carbohydrate picture and amount of carbohydrate
+ */
 const Glucides = (data) => {
   let keyData = data.data.keyData
   return (
     <div className="boite">
       <div className="iconDiv">
-        <img className="img" src={glucides} alt="proteines" />
+        <img className="img" src={glucides} alt="glucides" />
       </div>
       <div className="textDiv">
         <p className="textTop">{keyData.carbohydrateCount}g</p>
