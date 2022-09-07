62956fda8d61310914cec522764d7d3bf49276b9
citymer <cityhunter8484@gmail.com>
2022-09-07 17:33:55 +0200

    correction services

--- src/services/dataformatter.js
+++
@@ -1,8 +0,0 @@
-export class UserDataFormater {
-  constructor(mainData, activityData, averageSessionsData, performanceData) {
-    this.mainData = mainData
-    this.activity = activityData
-    this.averageSessions = averageSessionsData
-    this.performance = performanceData
-  }
-}
